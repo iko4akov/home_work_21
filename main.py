9de78b2d18dbd4139404ff62f902027e72647a81
iko4akov <iko4akov@gmail.com>
2023-04-07 21:51:39 +0300

    edit main.py

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from request import Request
+from exaptions import NotAmount, NotInListProduct
 
 
 def go():
-    text = input("Введите предложение: ")
-    while text.lower().strip() != 'стоп':
+    while True:
+
+        text = input("Введите предложение:")
+
+        if text.lower().strip() == "стоп":
+            break
+
         req = Request(text)
 
         request_dict = req.get_req_data()
@@ -24,23 +30,22 @@ def go():
         from_obj = from_data['class']
         to_obj = to_data['class']
 
-        if from_obj.remove(product, amount):
+        try:
+            from_obj.remove(product=product, amount=amount)
             if to_obj.add(product, amount):
                 print(f"Нужное количество есть на {request_dict['from']}")
                 print(f"Курьер забрал {amount} {product} со {request_dict['from']}a")
                 print(f"Курьер везет {amount} {product} со {request_dict.get('from')}а в {request_dict.get('to')}")
                 print(f"Курьер доставил {amount} {product} в {request_dict.get('to')}")
             else:
-                return print(f"В {to_data['name']} недостаточно места, попобуйте что то другое")
-        else:
-            return print(f'{from_data["name"]} нет в(на) {from_data["name"]}, попробуйте заказать меньше')
+                return print(f"В {to_data['name']} недостаточно места, попробуйте что то другое")
+
+        except (NotAmount, NotInListProduct) as error:
+            print(error.message)
 
         print(f"На {from_data['name']}е хранится:\n" + "\n".join(
             f"{item[0]}: {item[1]}" for item in from_obj.items.items()))
         print(f"В {to_data['name']}е хранится:\n" + "\n".join(f"{item[0]}: {item[1]}" for item in to_obj.items.items()))
-
-        text = input("Введите предложение: ")
-
 
 if __name__ == '__main__':
     go()
